1a09ca2713b019a77b2bcac89f0d6f99d839be5e
Karel Brezina <karel.brezina@gmail.com>
2012-12-11 16:30:27 +0100

    Initial implementation

---
+++ com.cloudsmith.puppet.modinfo/src/com/cloudsmith/puppet/modinfo/ForgeCallable.java
@@ -0,0 +1,146 @@
+package com.cloudsmith.puppet.modinfo;
+/**
+ * Copyright (c) 2012 Cloudsmith Inc. and other contributors, as listed below.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Cloudsmith
+ * 
+ */
+
+
+import hudson.FilePath.FileCallable;
+import hudson.remoting.VirtualChannel;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.cloudsmith.geppetto.forge.impl.MetadataImpl;
+import org.cloudsmith.geppetto.forge.v2.Forge;
+import org.cloudsmith.geppetto.forge.v2.client.ForgePreferences;
+import org.eclipse.jgit.storage.file.FileRepository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+public abstract class ForgeCallable<T> implements FileCallable<T> {
+	private static final long serialVersionUID = -3048930993120683688L;
+
+	public static final String IMPORTED_MODULES_ROOT = "importedModules";
+
+	public static final String BUILD_DIR = ".geppetto";
+
+	public static final Pattern DEFAULT_EXCLUDES_PATTERN;
+
+	static {
+		int top = MetadataImpl.DEFAULT_EXCLUDES.length;
+		String[] excludes = new String[top + 1];
+		System.arraycopy(MetadataImpl.DEFAULT_EXCLUDES, 0, excludes, 0, top);
+		excludes[top] = BUILD_DIR;
+		DEFAULT_EXCLUDES_PATTERN = MetadataImpl.compileExcludePattern(excludes);
+	}
+
+	private transient Forge forge;
+
+	private transient File repositoryDir;
+
+	private transient File buildDir;
+
+	private transient FileRepository localRepository;
+
+	private ForgePreferences forgePreferences;
+
+	private String repositoryURL;
+
+	private String branchName;
+
+	public ForgeCallable() {
+	}
+
+	public ForgeCallable(ForgePreferences forgePreferences, String repositoryURL, String branchName) {
+		this.forgePreferences = forgePreferences;
+		this.repositoryURL = repositoryURL;
+		this.branchName = branchName;
+	}
+
+	private boolean findModuleFiles(File[] files, List<File> moduleFiles) {
+		if(files != null) {
+			int idx = files.length;
+			while(--idx >= 0)
+				if("Modulefile".equals(files[idx].getName()))
+					return true;
+
+			idx = files.length;
+			while(--idx >= 0) {
+				File file = files[idx];
+				String name = file.getName();
+				if(DEFAULT_EXCLUDES_PATTERN.matcher(name).matches())
+					continue;
+
+				if(findModuleFiles(file.listFiles(), moduleFiles))
+					moduleFiles.add(file);
+			}
+		}
+		return false;
+	}
+
+	protected List<File> findModuleRoots() {
+		// Scan for valid directories containing "Modulefile" files.
+
+		List<File> moduleRoots = new ArrayList<File>();
+		if(findModuleFiles(repositoryDir.listFiles(), moduleRoots))
+			// The repository is a module in itself
+			moduleRoots.add(repositoryDir);
+		return moduleRoots;
+	}
+
+	public String getBranchName() {
+		return branchName;
+	}
+
+	protected File getBuildDir() {
+		return buildDir;
+	}
+
+	protected synchronized Forge getForge() {
+		if(forge == null)
+			forge = new Forge(forgePreferences);
+		return forge;
+	}
+
+	protected FileRepository getLocalRepository() throws IOException {
+		if(localRepository == null) {
+			File guessGitDir = new File(getRepositoryDir(), ".git");
+			boolean bare = !guessGitDir.isDirectory();
+			FileRepositoryBuilder bld = new FileRepositoryBuilder();
+			if(bare) {
+				bld.setBare();
+				bld.setGitDir(getRepositoryDir());
+			}
+			else
+				bld.setWorkTree(getRepositoryDir());
+			localRepository = bld.setup().build();
+		}
+		return localRepository;
+	}
+
+	protected File getRepositoryDir() {
+		return repositoryDir;
+	}
+
+	public String getRepositoryURL() {
+		return repositoryURL;
+	}
+
+	public final T invoke(File f, VirtualChannel channel) throws IOException, InterruptedException {
+		repositoryDir = f;
+		buildDir = new File(f, BUILD_DIR);
+		return invoke(channel);
+	}
+
+	protected abstract T invoke(VirtualChannel channel) throws IOException, InterruptedException;
+}
---
+++ com.cloudsmith.puppet.modinfo/src/com/cloudsmith/puppet/modinfo/ForgePublisher.java
@@ -0,0 +1,124 @@
+package com.cloudsmith.puppet.modinfo;
+/**
+ * Copyright (c) 2012 Cloudsmith Inc. and other contributors, as listed below.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Cloudsmith
+ * 
+ */
+
+
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.matrix.MatrixAggregatable;
+import hudson.matrix.MatrixAggregator;
+import hudson.matrix.MatrixRun;
+import hudson.matrix.MatrixBuild;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.plugins.git.GitSCM;
+import hudson.scm.SCM;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
+import hudson.tasks.Recorder;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class ForgePublisher extends Recorder implements Serializable, MatrixAggregatable {
+	@Extension(ordinal = -1)
+	public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+		@Override
+		public String getDisplayName() {
+			return "Puppet Forge Publishing";
+		}
+
+		@Override
+		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+			return true;
+		}
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	private final boolean publishOnlyIfSuccess;
+
+	@DataBoundConstructor
+	public ForgePublisher(Boolean publishOnlyIfSuccess) {
+		this.publishOnlyIfSuccess = publishOnlyIfSuccess == null
+				? true
+				: publishOnlyIfSuccess.booleanValue();
+	}
+
+	/**
+	 * For a matrix project, push should only happen once.
+	 */
+	public MatrixAggregator createAggregator(MatrixBuild build, Launcher launcher, BuildListener listener) {
+		return new MatrixAggregator(build, launcher, listener) {
+			@Override
+			public boolean endBuild() throws InterruptedException, IOException {
+				return ForgePublisher.this.perform(build, launcher, listener);
+			}
+		};
+	}
+
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.BUILD;
+	}
+
+	public boolean isPublishOnlyIfSuccess() {
+		return publishOnlyIfSuccess;
+	}
+
+	@Override
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, final BuildListener listener)
+			throws InterruptedException, IOException {
+
+		// during matrix build, the push back would happen at the very end only once for the whole matrix,
+		// not for individual configuration build.
+		if(build instanceof MatrixRun) {
+			return true;
+		}
+
+		List<ValidationResult> forgeValidators = build.getActions(ValidationResult.class);
+		if(forgeValidators.isEmpty()) {
+			listener.error("No Puppet Validation Result was found so no pushing will occur.");
+			return false;
+		}
+
+		final Result buildResult = build.getResult();
+
+		// If publishOnlyIfSuccess is selected and the build is not a success, don't push.
+		if(publishOnlyIfSuccess && buildResult.isWorseThan(Result.SUCCESS)) {
+			listener.getLogger().println(
+				"Build did not succeed and the project is configured to only push after a successful build, so no pushing will occur.");
+			return true;
+		}
+
+		SCM scm = build.getProject().getScm();
+		if(!(scm instanceof GitSCM)) {
+			listener.error("Unable to find Git SCM configuration in the project configuration");
+			return false;
+		}
+		GitSCM git = (GitSCM) scm;
+		FilePath gitRoot = git.getModuleRoot(build.getWorkspace(), build);
+		Diagnostic publishingResult = gitRoot.act(new ForgePublisherCallable(forgeValidators.get(0)));
+		for(Diagnostic diag : publishingResult.getChildren())
+			listener.getLogger().println(diag);
+
+		PublicationResult data = new PublicationResult(build, publishingResult);
+		build.addAction(data);
+		return publishingResult.getSeverity() < Diagnostic.ERROR;
+	}
+}
